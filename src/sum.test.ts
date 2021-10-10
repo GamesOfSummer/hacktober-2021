a10fb64620c96aca44857a83f91d4ade5777bdca
Summer T <summer@summertime-development.com>
2021-10-09 21:24:59 -0500

    install sinon

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,5 +1,11 @@
+import sinon, { assert } from 'sinon';
 import sum from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+it('returns the return value from the original function', function () {
+  var callback = sinon.fake.returns(42);
+  expect(callback()).toBe(42);
 });
